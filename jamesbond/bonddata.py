de3314094ba7539e9166165b6bb28ed3b30a0d49
andrewblock <ablock@bushelpowered.com>
2020-08-14 15:20:32 -0500

    Updated .gitignore and added new url to bonddata.py

--- jamesbond/bonddata.py
+++ jamesbond/bonddata.py
@@ -5,7 +5,7 @@ def load_data():
     """
     Bond movie data in pandas dataframe format.
     """
-    url = "https://raw.githubusercontent.com/andrew-block/james-bond-007/master/jamesbond/data/data.csv"
+    url = "https://raw.githubusercontent.com/andrew-block/jamesbond/master/data/data.csv"
     df = pd.read_csv(url)
 
     return df
