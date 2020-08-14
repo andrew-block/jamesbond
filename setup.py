63df657271e2f87098645e1273675c7d729b22b7
andrewblock <ablock@bushelpowered.com>
2020-08-14 16:05:53 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+with open('README.md') as readme_file:
+    README = readme_file.read()
 
-requirements = ["twine>=3", "wheel", "pip", "pandas"]
 
-setup(
-    name="jamesbond",
-    version="0.0.1",
-    author="Andrew Block",
-    author_email="andrewblock87@gmail.com",
-    description="James Bond tabular data package",
-    long_description=readme,
+setup_args = dict(
+    name='jamesbond',
+    version='0.0.12',
+    description='James Bond tabular data package in Pandas dataframe',
     long_description_content_type="text/markdown",
-    url="",
+    long_description=README,
+    license='MIT',
     packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
+    author='Andrew Block',
+    author_email='andrewblock87@gmail.com',
+    keywords=['Bond', 'JamesBond', 'James Bond', '007'],
+    url='https://github.com/andrew-block/jamesbond',
+    download_url='https://test.pypi.org/project/jamesbond/'
 )
+
+install_requires = [
+    'pandas>=1.1.0'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
